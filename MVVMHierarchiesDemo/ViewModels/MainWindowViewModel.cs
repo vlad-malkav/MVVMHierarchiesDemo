b4406ad5be507ba4afd7325546acaef67e4d3ad7
vlad-malkav <allanlerouge@gmx.fr>
2017-11-30 23:07:45 +0100

    First Commit

---
+++ MVVMHierarchiesDemo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMHierarchiesDemo.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+
+        private CustomerListViewModel custListViewModel = new CustomerListViewModel();
+
+        private OrderViewModel orderViewModelModel = new OrderViewModel();
+
+        private BindableBase _CurrentViewModel;
+
+        public BindableBase CurrentViewModel
+        {
+            get { return _CurrentViewModel; }
+            set { SetProperty(ref _CurrentViewModel, value); }
+        }
+
+        public MyICommand<string> NavCommand { get; private set; }
+
+        public MainWindowViewModel()
+        {
+            NavCommand = new MyICommand<string>(OnNav);
+        }
+
+        private void OnNav(string destination)
+        {
+
+            switch (destination)
+            {
+                case "orders":
+                    CurrentViewModel = orderViewModelModel;
+                    break;
+                case "customers":
+                default:
+                    CurrentViewModel = custListViewModel;
+                    break;
+            }
+        }
+    }
+}
